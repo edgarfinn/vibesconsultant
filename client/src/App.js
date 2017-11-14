60c1d795843136fb4f9ae025e934a9cec409b8b9
Edgar Dewsbery <edewsbery@gmail.com>
2017-11-14 14:30:54 +0000

    #2 hero title fix positioning and sizing

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import './Reset.css'
 
+{/* setup access to users route in express */}
 class App extends Component {
   state = {users: []}
-
 
 componentDidMount() {
   fetch('/users')
@@ -16,14 +16,15 @@ componentDidMount() {
     return (
       <div className="App">
         <section className="section-hero">
-          <header className="hero-header large-8 large-centered small-9 small-centered">
-            <h1 className="neon-font">Vibes Consultant</h1>
+          <header className="hero-header large-10 large-centered small-11">
+            <h1 className="neon-font">Vibes <br className="large-hide medium-show"/>Consultant</h1>
           </header>
           <p className="hero-description">Full site coming soon</p>
         </section>
-          {this.state.users.map(user =>
+        {/* access users route in express */}
+          {/* {this.state.users.map(user =>
             <div key={user.id}>{user.username}</div>
-          )}
+          )} */}
       </div>
     );
   }
